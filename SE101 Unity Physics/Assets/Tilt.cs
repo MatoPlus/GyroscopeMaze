2a1a6eea1e817cf392820c8c4577d1fc328d379e
Robert Craig <robertcraig38@gmail.com>
2019-10-30 13:14:51 -0400

    added gyro toggle for tilt.cs

--- SE101 Unity Physics/Assets/Tilt.cs
+++ SE101 Unity Physics/Assets/Tilt.cs
@@ -7,56 +7,80 @@ using System.Threading;
 
 public class Tilt : MonoBehaviour
 {
-    ///dev/ch.wchusbserial14410
-  // Start is called before the first frame update
-    SerialPort sp = new SerialPort("/dev/cu.wchusbserial14410", 9600);
+    // Public Variables (for changing in insepctor)
+    public bool useGyro = false;
+
+    // Private Variables
+    SerialPort sp;
     char[] teapotPacket = new char[14];  // InvenSense Teapot packet
     int serialCount = 0;                 // current packet byte position
     int synced = 0;
-    int interval = 0;
     float[] q = new float[4];
-    Quaternion quat = new Quaternion(1, 0, 0, 0);
+
     void Start()
     {
-        sp.Open();
+        // /dev/cu.wchusbserial14xx0   OR    COMx
+        if (useGyro)
+        {
+            sp = new SerialPort("/dev/cu.wchusbserial14430", 9600);
+            sp.Open();
+        }
     }
 
     void Update()
     {
-        while (sp.BytesToRead > 0)
+        // if useGyro is false use the 
+        if (!useGyro)
         {
-            int ch = sp.ReadByte();
-
-            if (synced == 0 && ch != '$') return;   // initial synchronization - also used to resync/realign if needed
-            synced = 1;
-            if ((serialCount == 1 && ch != 2)
-                || (serialCount == 12 && ch != '\r')
-                || (serialCount == 13 && ch != '\n'))
+            if (Input.GetAxis("Horizontal") > .2)
             {
-                serialCount = 0;
-                synced = 0;
-                return;
+                transform.Rotate(0, 0, 1);
             }
-
-            if (serialCount > 0 || ch == '$')
+            if (Input.GetAxis("Horizontal") < -.2)
             {
-                teapotPacket[serialCount++] = (char)ch;
-                if (serialCount == 14)
+                transform.Rotate(0, 0, -1);
+            }
+            if (Input.GetAxis("Vertical") > .2) {
+                transform.Rotate(1, 0 ,0);
+            }
+            if (Input.GetAxis("Vertical") < -.2) {
+                transform.Rotate(-1, 0 , 0);
+            }
+        }
+        else
+        {
+            while (sp.BytesToRead > 0)
+            {
+                int ch = sp.ReadByte();
+
+                if (synced == 0 && ch != '$') return;   // initial synchronization - also used to resync/realign if needed
+                synced = 1;
+                if ((serialCount == 1 && ch != 2)
+                    || (serialCount == 12 && ch != '\r')
+                    || (serialCount == 13 && ch != '\n'))
                 {
-                    serialCount = 0; // restart packet byte position
+                    serialCount = 0;
+                    synced = 0;
+                    return;
+                }
 
-                    // get quaternion from data packet
-                    q[0] = ((teapotPacket[2] << 8) | teapotPacket[3]) / 16384.0f;
-                    q[1] = ((teapotPacket[4] << 8) | teapotPacket[5]) / 16384.0f;
-                    q[2] = ((teapotPacket[6] << 8) | teapotPacket[7]) / 16384.0f;
-                    q[3] = ((teapotPacket[8] << 8) | teapotPacket[9]) / 16384.0f;
-                    for (int i = 0; i < 4; i++) if (q[i] >= 2) q[i] = -4 + q[i];
+                if (serialCount > 0 || ch == '$')
+                {
+                    teapotPacket[serialCount++] = (char)ch;
+                    if (serialCount == 14)
+                    {
+                        serialCount = 0; // restart packet byte position
 
-                    // set our toxilibs quaternion to new data
-                    //quat = new Quaternion(q[0], q[1], q[2], q[3]);
-                    transform.rotation = new Quaternion(q[2], q[0], -q[1], q[3]);
-                    //print("q:\t" + Math.Round(q[0] * 100.0f) / 100.0f + "\t" + Math.Round(q[1] * 100.0f) / 100.0f + "\t" + Math.Round(q[2] * 100.0f) / 100.0f + "\t" + Math.Round(q[3] * 100.0f) / 100.0f);
+                        // get quaternion from data packet
+                        q[0] = ((teapotPacket[2] << 8) | teapotPacket[3]) / 16384.0f;
+                        q[1] = ((teapotPacket[4] << 8) | teapotPacket[5]) / 16384.0f;
+                        q[2] = ((teapotPacket[6] << 8) | teapotPacket[7]) / 16384.0f;
+                        q[3] = ((teapotPacket[8] << 8) | teapotPacket[9]) / 16384.0f;
+                        for (int i = 0; i < 4; i++) if (q[i] >= 2) q[i] = -4 + q[i];
 
+                        // set our toxilibs quaternion to new data
+                        transform.rotation = new Quaternion(q[2], q[0], -q[1], q[3]);
+                    }
                 }
             }
         }
