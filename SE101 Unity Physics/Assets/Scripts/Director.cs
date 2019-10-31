6bc73fcae5f58b15814746bda64fb26d3cc4c2c5
Ibraheem Aboulnaga <ibraheem.aboulnaga@uwaterloo.ca>
2019-10-31 17:24:49 -0400

    potentially working? added maze generator

--- SE101 Unity Physics/Assets/Scripts/Director.cs
+++ SE101 Unity Physics/Assets/Scripts/Director.cs
@@ -8,53 +8,71 @@ public class Director : MonoBehaviour {
     public GameObject WallPrefab;
     public GameObject PlatformPrefab;
     public GameObject Platform;
+    public GameObject TilePrefab;
+    public GameObject Tile;
 
     void Awake()
     {
         BallPrefab = (GameObject)Resources.Load("Prefabs/Ball");
         WallPrefab = (GameObject)Resources.Load("Prefabs/Wall");
+        //TilePrefab = (GameObject)Resources.Load("Prefabs/PlatformTile");
         PlatformPrefab = (GameObject)Resources.Load("Prefabs/Platform");
     }
     // Use this for initialization
     void Start () {
-        Ball = Instantiate(BallPrefab, new Vector3(0, 1, 0), Quaternion.identity);
-        MakeLevel("box");
+        boxPreset = new int[3, 10, 10];
+        for (int i = 0; i < 10; i++)
+        {
+            boxPreset[0, 0, i] = 1;
+            boxPreset[0, 9, i] = 1;
+            boxPreset[0, 1, i/2] = 1;
+            boxPreset[1, i, 0] = 1;
+            boxPreset[1, i, 9] = 1;
+            for (int j = 0; j < 10; j++)
+            {
+                boxPreset[2, i, j] = 1;
+            }
+            boxPreset[2, 7, 7] = 0;
+        }
+        MakeLevel(10,10,boxPreset);
+        Ball = Instantiate(BallPrefab, new Vector3(0, (float)1.5, 0), Quaternion.identity);
     }
 
-    public GameObject[] wallsArray;
-    public int numWalls;
-    void MakeLevel(string template)
-    {
+    public GameObject[,,] wallsArray;
+    public GameObject[,] platsArray;
+    public int[,,] boxPreset;
+    private Vector3 origin;
+    private int numWalls;
+    void MakeLevel(int width, int height, int [,,] toGenerate){
+        //first index in toGenerate is horizontal or vertical (0 is horiz., 1 is vert.)
+        //2nd index is height, 3rd is width
+        origin = new Vector3((float)(-width/2),0,(float)(-height/2+1));
         Platform = Instantiate(PlatformPrefab, new Vector3(0, 0, 0), Quaternion.identity);
-        if (template == "box")
-        {
-            numWalls = 40;
-            wallsArray = new GameObject[numWalls];
-            for (int i = 0; i < 10; i++)
-            {
-                GameObject wall = Instantiate(WallPrefab, new Vector3((float)(i - 4.5), (float)0.5, -5), Quaternion.identity);
-                wall.transform.SetParent(Platform.transform);
-                wallsArray[i] = wall;
+        /*Tile = Instantiate(TilePrefab, new Vector3(0, 0, 0), Quaternion.identity);
+        Tile.transform.SetParent(Platform.transform);
+        Tile.transform.localScale = new Vector3(1,1,1);*/
+        wallsArray = new GameObject[3,height, width];
+        for (int i = 0; i < height; i++){
+            for(int j = 0; j < width; j++){
+                if(toGenerate[0,i,j] == 1){
+                    GameObject wall = Instantiate(WallPrefab, new Vector3(origin.x+(float)j, (float)0.5, origin.z+(float)i), Quaternion.identity);
+                    wall.transform.SetParent(Platform.transform);
+                    wallsArray[0,i,j] = wall;
+                }
+                if (toGenerate[1, i, j] == 1)
+                {
+                    GameObject wall = Instantiate(WallPrefab, new Vector3(origin.x + (float)j, (float)0.5, origin.z + (float)i), Quaternion.Euler(0, 90, 0));
+                    wall.transform.SetParent(Platform.transform);
+                    wallsArray[1,i, j] = wall;
+                }
+                /*if (toGenerate[2, i, j] == 1)
+                {
+                    GameObject tile = Instantiate(TilePrefab, new Vector3(origin.x + (float)j, (float)0, origin.z + (float)i), Quaternion.Euler(90, 0, 90));
+                    tile.transform.SetParent(Platform.transform);
+                    wallsArray[2, i, j] = tile;
+                }*/
             }
-            for (int i = 0; i < 10; i++)
-            {
-                GameObject wall = Instantiate(WallPrefab, new Vector3((float)(i - 4.5), (float)0.5, 5), Quaternion.identity);
-                wall.transform.SetParent(Platform.transform);
-                wallsArray[i+10] = wall;
-            }
-            for (int i = 0; i < 10; i++)
-            {
-                GameObject wall = Instantiate(WallPrefab, new Vector3((float)-5, (float)0.5, (float)(i-4.5)), Quaternion.Euler(0,90,0));
-                wall.transform.SetParent(Platform.transform);
-                wallsArray[i+20] = wall;
-            }
-            for (int i = 0; i < 10; i++)
-            {
-                GameObject wall = Instantiate(WallPrefab, new Vector3((float)5, (float)0.5, (float)(i-4.5)), Quaternion.Euler(0, 90, 0));
-                wall.transform.SetParent(Platform.transform);
-                wallsArray[i+30] = wall;
-            }
-        }
+        }        
     }
 	// Update is called once per frame
 	void Update () {
--- SE101 Unity Physics/Assets/Scripts/Tilt.cs
+++ SE101 Unity Physics/Assets/Scripts/Tilt.cs
@@ -1,40 +1,103 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
+using System.IO.Ports;
+using System.Threading;
 
 public class Tilt : MonoBehaviour
 {
+    // Public Variables (for changing in insepctor)
+    public bool useGyro = false;
 
-    // Use this for initialization
+    // Private Variables
+    SerialPort sp;
+    char[] teapotPacket = new char[14];  // InvenSense Teapot packet
+    int serialCount = 0;                 // current packet byte position
+    int synced = 0;
+    float[] q = new float[4];
+
     void Start()
     {
-
+        // /dev/cu.wchusbserial14xx0   OR    COMx
+        if (useGyro)
+        {
+            SetUpGyro();
+        }
     }
 
-    // Update is called once per frame
     void Update()
     {
-        if (Input.GetAxis("Horizontal") > .2)
+        // if useGyro is false use the 
+        if (!useGyro)
         {
-            transform.Rotate(0, 0, 1);
-
+            if (Input.GetAxis("Horizontal") > .2)
+            {
+                transform.Rotate(0, 0, 1);
+            }
+            if (Input.GetAxis("Horizontal") < -.2)
+            {
+                transform.Rotate(0, 0, -1);
+            }
+            if (Input.GetAxis("Vertical") > .2)
+            {
+                transform.Rotate(1, 0, 0);
+            }
+            if (Input.GetAxis("Vertical") < -.2)
+            {
+                transform.Rotate(-1, 0, 0);
+            }
         }
-        if (Input.GetAxis("Horizontal") < -.2)
+        else
         {
-            transform.Rotate(0, 0, -1);
 
+            if (sp == null)
+            {
+                SetUpGyro();
+            }
 
+            while (sp.BytesToRead > 0)
+            {
+                int ch = sp.ReadByte();
+
+                if (synced == 0 && ch != '$') return;   // initial synchronization - also used to resync/realign if needed
+                synced = 1;
+                if ((serialCount == 1 && ch != 2)
+                    || (serialCount == 12 && ch != '\r')
+                    || (serialCount == 13 && ch != '\n'))
+                {
+                    serialCount = 0;
+                    synced = 0;
+                    return;
+                }
+
+                if (serialCount > 0 || ch == '$')
+                {
+                    teapotPacket[serialCount++] = (char)ch;
+                    if (serialCount == 14)
+                    {
+                        serialCount = 0; // restart packet byte position
+
+                        // get quaternion from data packet
+                        q[0] = ((teapotPacket[2] << 8) | teapotPacket[3]) / 16384.0f;
+                        q[1] = ((teapotPacket[4] << 8) | teapotPacket[5]) / 16384.0f;
+                        q[2] = ((teapotPacket[6] << 8) | teapotPacket[7]) / 16384.0f;
+                        q[3] = ((teapotPacket[8] << 8) | teapotPacket[9]) / 16384.0f;
+                        for (int i = 0; i < 4; i++) if (q[i] >= 2) q[i] = -4 + q[i];
+
+                        // set our toxilibs quaternion to new data
+                        transform.rotation = new Quaternion(q[2], q[0], -q[1], q[3]);
+                    }
+                }
+            }
         }
-        if (Input.GetAxis("Vertical") > .2)
-        {
-            transform.Rotate(1, 0, 0);
-
-        }
-        if (Input.GetAxis("Vertical") < -.2)
-        {
-            transform.Rotate(-1, 0, 0);
+    }
 
 
-        }
+    void SetUpGyro()
+    {
+        // sp = new SerialPort("/dev/cu.wchusbserial14430", 9600);
+        sp = new SerialPort("COM6", 9600);
+        sp.Open();
     }
 }
