4a4a0abb8ad2aec0c815fa5fd44a77c7ca0aff40
Ibraheem Aboulnaga <ibraheem.aboulnaga@uwaterloo.ca>
2019-11-01 19:51:23 -0400

    revised comments for clarification

--- SE101 Unity Physics/Assets/Scripts/Director.cs
+++ SE101 Unity Physics/Assets/Scripts/Director.cs
@@ -24,9 +24,11 @@ public class Director : MonoBehaviour {
         for (int i = 0; i < 10; i++)
         {
             boxPreset[1, 0, i] = 1;
-            boxPreset[0, i, 2] = 1;
+            boxPreset[0, (i / 2), 2] = 1;
+            boxPreset[0, 9-(i / 2), 4] = 1;
             boxPreset[1, 10, i] = 1;
             boxPreset[0, i, 0] = 1;
+            boxPreset[0, i, 10] = 1;
             boxPreset[0, i, 10] = 1;
             for (int j = 0; j < 10; j++)
             {
@@ -45,17 +47,21 @@ public class Director : MonoBehaviour {
     The origin is at the bottom left of the maze; x goes right, y goes up. Currently, the platform size doesn't scale properly, but I will fix this. 
     (Don't worry about setting the origin; the maze as a whole will always have its center at 0,0,0 in the game world.)
     Index 0 (Type): 0 is horizontal walls, 1 is vertical walls. 2 is currently unused, but required (place a dummy value at 2)
+    Horizontal walls start at the X,Y coordinate and continue 1 unit right, while vertical walls continue up.
     Index 1 (X): Should be an array of [[width]] arrays; the Y arrays. Each index corresponds to a "column," 0 being the leftmost.
     Index 1 (Y): Should be an array of [[height]] 0's and 1's. If a value is 1, a wall will be placed there, else no wall. Each index corresponds to a "row", 0 being the lowermost.
-    Basically, two 2D arrays; the first indexed at 0, 
+    Basically, two 2D arrays; the first indexed at 0, the other indexed at 1.
     e.g. array[0,0,0] = 1 means a horizontal wall will be placed at position 0,0 on the grid.
-    e.g. array[1,2,3] = 1 means a vertical wall will be placed at position 2,3 on the grid (2 right, 3 up)  
+    e.g. array[1,2,3] = 1 means a vertical wall will be placed at position 2,3 on the grid (2 right, 3 up) 
+    Please note: The blocks are 1 unit long, so a vertical wall placed at the very highest y value will reach past a horizontal wall placed at the highest y value. When generating a maze, it's best to keep this in mind 
+    (e.g., for a maze of height 11, only place 10 vertical walls on the left and right borders (indices 0 through 9), and the wall at height 9 will touch the horizontal wall that has been placed at height 10, since it extends up one unit)
      */
     void MakeLevel(int [,,] toGenerate){
         int width = toGenerate.GetLength(1);
         int height = toGenerate.GetLength(2);
         Vector3 origin = new Vector3((float)(-width/2),0,(float)(-height/2));
         Platform = Instantiate(PlatformPrefab, new Vector3(0, 0, 0), Quaternion.identity);
+        Platform.transform.localScale = new Vector3(width-1, (float)0.1, height-1);
         /*Tile = Instantiate(TilePrefab, new Vector3(0, 0, 0), Quaternion.identity);
         Tile.transform.SetParent(Platform.transform);
         Tile.transform.localScale = new Vector3(1,1,1);*/
