5781507ac888a30e614a764a6efe4c7d7bee17ff
Ibraheem Aboulnaga <ibraheem.aboulnaga@uwaterloo.ca>
2019-11-12 14:41:46 -0500

    starting physics refactor

---
+++ SE101 Unity Physics/Assets/Scripts/FakeGravity.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FakeGravity : MonoBehaviour {
+    public Vector3 grav;
+    public float magnitude;
+	// Use this for initialization
+	void Awake () {
+        magnitude = 9.81f;
+        grav = Vector3.down*magnitude;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void FixedUpdate()
+    {
+        this.GetComponent<Rigidbody>().AddForce(grav, ForceMode.Acceleration);
+    }
+}
