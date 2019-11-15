329eaf1b4489f29016bf3f65acc1b0c2573f27f6
MatoPlus <rixin.yang.2001@gmail.com>
2019-11-15 15:54:28 -0500

    changed naming scheme

--- SE101 Unity Physics/Assets/Scripts/Tilt.cs
+++ SE101 Unity Physics/Assets/Scripts/Tilt.cs
@@ -15,15 +15,14 @@ public class Tilt : MonoBehaviour
 
     // Private Variables
     SerialPort sp;
-    char[] teapotPacket = new char[14];  // InvenSense Teapot packet
+    char[] packet = new char[14];  // InvenSense Teapot packet
     int serialCount = 0;                 // current packet byte position
     int synced = 0;
     float[] q = new float[4];
 
     void Start()
     {
-        useGyro = false;
-        // /dev/cu.wchusbserial14xx0   OR    COMx
+        useGyro = true;
         if (useGyro)
         {
             SetupController();
@@ -80,16 +79,16 @@ public class Tilt : MonoBehaviour
 
                 if (serialCount > 0 || ch == '$')
                 {
-                    teapotPacket[serialCount++] = (char)ch;
+                    packet[serialCount++] = (char)ch;
                     if (serialCount == 14)
                     {
                         serialCount = 0; // restart packet byte position
 
                         // get quaternion from data packet
-                        q[0] = ((teapotPacket[2] << 8) | teapotPacket[3]) / 16384.0f;
-                        q[1] = ((teapotPacket[4] << 8) | teapotPacket[5]) / 16384.0f;
-                        q[2] = ((teapotPacket[6] << 8) | teapotPacket[7]) / 16384.0f;
-                        q[3] = ((teapotPacket[8] << 8) | teapotPacket[9]) / 16384.0f;
+                        q[0] = ((packet[2] << 8) | packet[3]) / 16384.0f;
+                        q[1] = ((packet[4] << 8) | packet[5]) / 16384.0f;
+                        q[2] = ((packet[6] << 8) | packet[7]) / 16384.0f;
+                        q[3] = ((packet[8] << 8) | packet[9]) / 16384.0f;
                         for (int i = 0; i < 4; i++) if (q[i] >= 2) q[i] = -4 + q[i];
 
                         // set our toxilibs quaternion to new data
