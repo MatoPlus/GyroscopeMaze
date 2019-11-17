034da5c98177573650d724504a20fa7383011f90
Kevin Li <kevin1@v1040-wn-rt-b-3-208.campus-dynamic.uwaterloo.ca>
2019-11-17 17:47:49 -0500

    Serial port dropdown completed, but needs to filter out irrelevant ports

---
+++ SE101 Unity Physics/Assets/Scripts/SerialDropdown.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using System.IO.Ports;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SerialDropdown : MonoBehaviour {
+
+    public Dropdown dropdown;
+
+    void Start () {
+        PopulateList();	
+	}
+	
+	// Update is called once per frame
+	void PopulateList() {
+        List<string> ports = new List<string>(SerialPort.GetPortNames());
+        dropdown.AddOptions(ports);
+    }
+}
