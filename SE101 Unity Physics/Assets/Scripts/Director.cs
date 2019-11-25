c6d66f77453f9f3bfd4338ed829024509b02279e
Ibraheem Aboulnaga <ibraheem.aboulnaga@uwaterloo.ca>
2019-11-25 16:14:49 -0500

    we done boysss

--- SE101 Unity Physics/Assets/Scripts/Director.cs
+++ SE101 Unity Physics/Assets/Scripts/Director.cs
@@ -186,8 +186,6 @@ public class Director : MonoBehaviour
     public void QuitGame()
     {
         #if UNITY_EDITOR
-                // Application.Quit() does not work in the editor so
-                // UnityEditor.EditorApplication.isPlaying need to be set to false to end the game
                 UnityEditor.EditorApplication.isPlaying = false;
         #else
                  Application.Quit();
